c65d24989b529d3e668699a881eb06e80bfd08ca
willzfrank <willzudoh4@gmail.com>
2022-03-02 07:32:38 +0100

    Navigation Desktop view Done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import React from "react";
 
-import './Navbar.css';
+import "./Navbar.css";
 
-const Navbar = () => (
-  <div>
-    Navbar
-  </div>
-);
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="nav__mainContainer">
+        <div className="nav__mainContainer--logo">La Cuisine</div>
+        <div>
+          <ul className="nav__mainContainer--linkContainer">
+            <li>
+              <a href="#home">Home</a>
+            </li>
+            <li>
+              <a href="#pages">Pages</a>
+            </li>
+            <li>
+              <a href="#contactus">Contact Us</a>
+            </li>
+            <li>
+              <a href="#blog">Blog</a>
+            </li>
+            <li>
+              <a href="#landing">Landing</a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <div className="nav__mainContainer--loginContainerBox">
+            <div className="nav__mainContainer--login">
+              <a href="#login">Log in / Registration</a>
+            </div>
+
+            <div className="nav__mainContainer--loginDivider"></div>
+            <div className="nav__mainContainer--login">
+              <a href="#book">Book Table</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
+import SubHeading from "../../components/SubHeading/SubHeading";
+import "./Header.css";
 
-import './Header.css';
-
-const Header = () => (
-  <div>
-    Header
-  </div>
-);
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <SubHeading />
+      </div>
+    </header>
+  );
+};
 
 export default Header;
