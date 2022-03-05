49251ce27ad842258ffd3b0662b49d7b25f375e7
willzfrank <willzudoh4@gmail.com>
2022-03-05 09:02:47 +0100

    chef X menu completed

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,11 +1,47 @@
-import React from 'react';
+import React from "react";
+import SubHeading from "../../components/SubHeading/SubHeading";
+import { images } from "../../constants";
 
-import './Chef.css';
+import "./Chef.css";
 
 const Chef = () => (
-  <div>
-    Chef
-  </div>
+  <section className="chef__container--box">
+    <div className="chef__container--chefimagebox">
+      <img
+        src={images.chef}
+        alt="la-cuisine"
+        className="chef__container--imagechef"
+      />
+    </div>
+    <div>
+      <div>
+        <SubHeading title="Chef's Word" />
+        <h2>What We Believe In</h2>
+      </div>
+      <div className="chef__container--quotebox">
+        <div className="chef__container--quote">
+          <img src={images.quote} alt="la-cuisine" />
+        </div>
+        <div className="chef__container--quotetextbox">
+          <p className="chef__container--quotetext flex__end">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa
+            soluta nulla facilis.
+          </p>
+        </div>
+        <p className="chef__container--quotetext">
+          quam modi, porro ipsa obcaecati,Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Voluptas necessitatibus similique quod. Nostrum
+          totam veritatis excepturi est ipsa vitae, eveniet cupiditate veniam
+          ullam ea, perferendis doloribus molestiae eos ipsum molestias.
+        </p>
+      </div>
+      <div className="chef__container--name">
+        <p>Kevin Luo</p>
+        <p className="chef__container--quotetext">Chef & Founder</p>
+        <img src={images.sign} alt="Signature" />
+      </div>
+    </div>
+  </section>
 );
 
 export default Chef;
